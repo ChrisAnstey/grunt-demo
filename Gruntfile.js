7d45b9617707310fe08f93635ed96949d100d369
Chris Anstey <chris@cpanstey.co.uk>
2014-02-20 18:40:06 +0000

    add watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,24 @@ module.exports = function(grunt) {
               banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
 
-        dist: {
-            src: [
-                'js/scripts.js', 
-                'js/jquery.js'  
-            ],
-            dest: 'js/production.js',
-            }
-        }
+            dist: {
+                src: [
+                    'js/scripts.js', 
+                    'js/jquery.js'  
+                ],
+                dest: 'js/production.js',
+            },
+
+        },
+        watch: {
+            files: ['**/scripts.js'],
+            tasks: ['concat'],
+        },
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['concat']);
 
