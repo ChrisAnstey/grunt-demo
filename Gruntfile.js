752a4f9aa663105727fa17b98e3ecd30b60364cf
Chris Anstey <chris@cpanstey.co.uk>
2014-02-20 18:31:44 +0000

    tidy up indents

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,18 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         concat: {
-    options: {
-      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    },
+            options: {
+              banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
 
-		dist: {
-			src: [
-			    'js/scripts.js', 
-			    'js/jquery.js'  
-			],
-			dest: 'js/production.js',
-		    }
-		}
+        dist: {
+            src: [
+                'js/scripts.js', 
+                'js/jquery.js'  
+            ],
+            dest: 'js/production.js',
+            }
+        }
 
     });
 
